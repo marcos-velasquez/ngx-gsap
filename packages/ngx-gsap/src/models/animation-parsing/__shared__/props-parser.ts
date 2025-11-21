0f37397de12a65a96bd922e0df3e108fff744d89
Marcos Velasquez <marcos@socialgest.net>
2025-11-21 17:29:49 -0400

    ``` feat: add quote handling to PropsParser for proper string value parsing
    - Added inQuotes and quoteChar state tracking to PropsParser class
    - Implemented isQuote() method to detect single and double quotes
    - Added toggleQuote() method to track quote state and matching quote characters
    - Updated isOpenBrace() and isCloseBrace() checks to ignore braces inside quoted strings
    - Updated isAssignment() and isSeparator() checks to ignore delimiters inside quoted strings
    - Modified parse loop to handle quote characters an

--- packages/ngx-gsap/src/models/animation-parsing/__shared__/props-parser.ts
+++ packages/ngx-gsap/src/models/animation-parsing/__shared__/props-parser.ts
@@ -1,6 +1,9 @@
 import { TypeConverter, ObjectParser, BaseParser } from '../../../utils';
 
 export class PropsParser extends BaseParser {
+  private inQuotes = false;
+  private quoteChar = '';
+
   constructor(propsString: string) {
     super(propsString);
   }
@@ -13,14 +16,21 @@ export class PropsParser extends BaseParser {
     while (!this.isAtEnd()) {
       const char = this.current();
 
-      if (this.isOpenBrace(char)) {
+      if (this.isQuote(char)) {
+        this.toggleQuote(char);
+        currentValue += char;
+        this.next();
+        continue;
+      }
+
+      if (this.isOpenBrace(char) && !this.inQuotes) {
         this.enterNested();
         currentValue += char;
         this.next();
         continue;
       }
 
-      if (this.isCloseBrace(char)) {
+      if (this.isCloseBrace(char) && !this.inQuotes) {
         this.exitNested();
         currentValue += char;
         this.next();
@@ -55,12 +65,26 @@ export class PropsParser extends BaseParser {
     return props;
   }
 
+  private isQuote(char: string): boolean {
+    return char === '"' || char === "'";
+  }
+
+  private toggleQuote(char: string): void {
+    if (!this.inQuotes) {
+      this.inQuotes = true;
+      this.quoteChar = char;
+    } else if (char === this.quoteChar) {
+      this.inQuotes = false;
+      this.quoteChar = '';
+    }
+  }
+
   private isAssignment(char: string, currentKey: string): boolean {
-    return char === '=' && this.isAtRootLevel() && !currentKey;
+    return char === '=' && this.isAtRootLevel() && !currentKey && !this.inQuotes;
   }
 
   private isSeparator(char: string): boolean {
-    return char === ',' && this.isAtRootLevel();
+    return char === ',' && this.isAtRootLevel() && !this.inQuotes;
   }
 
   private parseValue(value: string): unknown {
