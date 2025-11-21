9227bb63fa5aa68e0d0092c903b8c130f7440afd
Marcos Velasquez <marcos@socialgest.net>
2025-11-21 00:08:23 -0400

    ``` feat: add SplitTextApplicator with placeholder implementation
    - Created SplitTextApplicator class implementing AnimationApplicator interface
    - Added TODO comment for future implementation when splitTextVars is added to AnimationParserResult
    - Included commented code showing intended usage of timeline.splitText() method
    ```

---
+++ packages/ngx-gsap/src/models/animation-parsing/applicators/split-text-applicator.ts
@@ -0,0 +1,12 @@
+import { Timeline } from '../../timeline';
+import { AnimationParserResult } from '../animation-parser';
+import { AnimationApplicator } from './animation-applicator';
+
+export class SplitTextApplicator implements AnimationApplicator {
+  public apply(_timeline: Timeline, _context: AnimationParserResult): void {
+    // TODO: Implementar cuando se agregue splitTextVars al AnimationParserResult
+    // if (_context.splitTextVars) {
+    //   _timeline.splitText(_context.splitTextVars)(_timeline.timeline);
+    // }
+  }
+}
